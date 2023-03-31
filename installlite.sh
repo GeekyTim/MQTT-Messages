1ab7b628d60e94237799574f934f5b6adfdbd018
Tim Richardson <software@potton.me.uk>
2023-03-31 18:59:11 +0100

    Altered installation

--- installlite.sh
+++ installlite.sh
@@ -2,10 +2,6 @@
 
 scriptpath=${BASH_SOURCE[0]}
 
-sudo apt-get update
-sudo apt-get install python3-pip -y
-pip3 install paho.mqtt jsondict
-
 cd "$scriptpath"/library || exit
 sudo python3 setup.py install
 cd "$scriptpath" || exit
